08f9f40fcf430baf13290a211a34f7cd8232e0f3
Naresh <naresh@archistar.ai>
2019-12-04 23:22:32 +1100

    Initial Commit

--- src/_config/URLConfig.js
+++ src/_config/URLConfig.js
@@ -1,10 +1,10 @@
 export const URL_CONFIG = {
   "mailgun" : {
     "method" : "POST",
-    "url" : "http://sitemindermailservermailgun-env.gqdmudnp5w.us-east-2.elasticbeanstalk.com/"
+    "url" : "https://sitemindermailservermailgun-env.gqdmudnp5w.us-east-2.elasticbeanstalk.com/"
   },
   "sendgrid" : {
     "method" : "POST",
-    "url" : "http://sitemindermailserversendgrid-env.wcczputw3f.us-east-2.elasticbeanstalk.com"
+    "url" : "https://sitemindermailserversendgrid-env.wcczputw3f.us-east-2.elasticbeanstalk.com"
   }
 };
